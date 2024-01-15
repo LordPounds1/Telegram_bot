fe577cebc4d46b5c7419ced29e1eaf41d4319bf5
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2024-01-15 19:14:50 +0600

    Добавление Базы данных

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import telebot
 import webbrowser
+import sqlite3
 from telebot import types
 
 bot = telebot.TeleBot('6519856683:AAHy5KDNzXzSF_Fj83a7SzNZbzoP9eYKjOA')
+name = None
 
 @bot.message_handler(commands=['start'])
 
@@ -26,6 +28,61 @@ def on_click(message):
     elif message.text == 'Удалить фото':
         bot.send_message(message.chat.id, 'Delete')
 
+
+@bot.message_handler(commands=['baza'])
+
+def baza(message):
+    conn = sqlite3.connect('baza.sql')
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name varchar(50), pass varchar(50))")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id,'Здорово уебок, как тебя зовут?')
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введи пароль блядота')
+    bot.register_next_step_handler(message, user_pass)
+
+def user_pass(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('baza.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users(name, pass) VALUES('%s', '%s')" %(name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Список уебков', callback_data='users'))
+    bot.send_message(message.chat.id, 'Congratulations!',reply_markup=markup)
+    #bot.register_next_step_handler(message,user_pass)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+
+def call_back(call):
+    conn = sqlite3.connect('baza.sql')
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    info = ''
+
+    for el in users:
+        info += f'Имя: {el[1]}, Пароль: {el[2]} \n'
+
+
+    cur.close()
+    conn.close()
+
+    bot.send_message(call.message.chat.id, info)
 
 
 @bot.message_handler(commands=['site','website'])
